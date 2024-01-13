3f3d1d3fe359ac5feedac780a8126e383336c90f
atarek477 <atarek4777@gmail.com>
2024-01-13 05:21:01 +0200

    ui using charkra template

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,18 +1,16 @@
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SidebarWithHeader from './shared/slideBAR.jsx'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
-return (
-<div>
-<h1>front-end</h1>
 
-<ol>
-<li>React</li>
-<li>angular</li>
-<li>Vue</li>
-<li>Native</li>
 
-</ol>
-</div>)
+  return ( <SidebarWithHeader>
+                <Button colorScheme='blue'>Button</Button>
+              </SidebarWithHeader>)
 }
 
 export default App
